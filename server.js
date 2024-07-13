1bc3e91df3a12a03baa66b82dec92c0bb69b325c
publicus <tulaba@gmail.com>
2024-07-13 14:08:53 +0300

    Update configuration for Vercel deployment

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
-app.get('/api/categories.json', (req, res) => {
+
+app.get('/categories.json', (req, res) => {
   const updatedPath = path.join(__dirname, 'public', 'categories_updated.json');
   const defaultPath = path.join(__dirname, 'public', 'categories.json');
 
@@ -29,7 +30,7 @@ app.get('/api/categories.json', (req, res) => {
   });
 });
 
-app.get('/api/categories_updated.json', (req, res) => {
+app.get('/categories_updated.json', (req, res) => {
   fs.readFile(path.join(__dirname, 'public', 'categories_updated.json'), 'utf8', (err, data) => {
     if (err) {
       console.error('Error reading categories_updated.json:', err);
@@ -40,7 +41,7 @@ app.get('/api/categories_updated.json', (req, res) => {
   });
 });
 
-app.post('/api/save_categories', (req, res) => {
+app.post('/save_categories', (req, res) => {
   const categories = req.body;
   fs.writeFile(path.join(__dirname, 'public', 'categories_updated.json'), JSON.stringify(categories, null, 2), (err) => {
     if (err) {
@@ -53,7 +54,7 @@ app.post('/api/save_categories', (req, res) => {
   });
 });
 
-app.post('/api/clear_categories', (req, res) => {
+app.post('/clear_categories', (req, res) => {
   fs.writeFile(path.join(__dirname, 'public', 'categories_updated.json'), JSON.stringify([], null, 2), (err) => {
     if (err) {
       console.error('Error clearing categories_updated.json:', err);
